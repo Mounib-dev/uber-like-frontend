777ea0cc173f791736615f3d52c2742fd9bf98ce
adaG99 <bebead14@gmail.com>
2025-04-28 15:42:43 +0200

    add footer and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Login from "./components/auth/Login";
 // import PrivateRoute from "./components/auth/PrivateRoute";
 import Home from "./components/Home";
+import Navbar from "./components/layout/Navbar";  
+import Footer from "./components/layout/Footer";  
 
 function App() {
   return (
-    <>
+    <Router>
       <div className="flex min-h-screen flex-col">
-        <Router>
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              {/* <Route path="/login" element={<Login />} /> */}
-              {/* <Route element={<PrivateRoute />}></Route> */}
-            </Routes>
-          </main>
-        </Router>
+        <Navbar /> 
+
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/login" element={<Login />} /> */}
+            {/* <Route element={<PrivateRoute />}></Route> */}
+          </Routes>
+        </main>
+
+        <Footer /> 
       </div>
-    </>
+    </Router>
   );
 }
 
