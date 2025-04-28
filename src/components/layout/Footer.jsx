8c30fc4a3feb98f1daec2e181ad333aa138f81fa
adaG99 <bebead14@gmail.com>
2025-04-28 15:41:43 +0200

    add footer

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer className="bg-gray-100 p-4 text-center text-gray-500">
+        &copy; {new Date().getFullYear()} UberEatApp. Tous droits réservés.
+      </footer>
+    );
+  }
+  
