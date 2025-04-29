b033e3371edce56517d517e08a36de9dcf319809
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-04-29 15:09:35 +0200

    Wrap application within AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import { AuthProvider } from "./context/AuthContext";
- import Login from "./components/auth/Login";
- import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 // import PrivateRoute from "./components/auth/PrivateRoute";
 import Home from "./components/Home";
-import Navbar from "./components/layout/Navbar";  
-import Footer from "./components/layout/Footer";  
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 import PlaceOrderForm from "./components/orders/PlaceOrder";
 import Menu from "./components/restaurants/Menu";
 import RestaurantList from "./components/restaurants/RestaurantsList";
 import Panier from "./components/orders/Panier";
 
+import PrivateRoute from "./components/auth/Private";
+import { AuthProvider } from "./context/AuthContext";
+
 function App() {
   return (
     <Router>
       <div className="flex min-h-screen flex-col">
-        <Navbar /> 
+        <AuthProvider>
+          <Navbar />
 
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-             <Route path="/login" element={<Login />} /> 
-             <Route path="/register" element={<Register />} /> 
-             <Route path="/order" element={<PlaceOrderForm />} /> 
-             <Route path="/menu" element={<Menu />} /> 
-             <Route path="/restaurants" element={<RestaurantList />} /> 
-             <Route path="/panier" element={<Panier />} /> 
-            {/* <Route element={<PrivateRoute />}></Route> */}
-          </Routes>
-        </main>
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
 
-        <Footer /> 
+              {/* <Route element={<PrivateRoute />}></Route> */}
+              <Route element={<PrivateRoute />}>
+                <Route path="/order" element={<PlaceOrderForm />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/restaurants" element={<RestaurantList />} />
+                <Route path="/panier" element={<Panier />} />
+              </Route>
+            </Routes>
+          </main>
+        </AuthProvider>
+        <Footer />
       </div>
     </Router>
   );
